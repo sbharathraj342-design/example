fa8c9cbdc15e3060035e1f1dea8aca03312c2c35
bharathraj <sbharathraj342@gmail.com>
2026-05-31 11:20:00 +0530

    comment lines

--- emc.py
+++ emc.py
@@ -1,74 +1,74 @@
-s = {1,2,2,3,"python"}
-for element in s:
-    print(element)  
+# s = {1,2,2,3,"python"}
+# for element in s:
+#     print(element)  
 
-tup = (1,2,3,4)
-print(tup[0])
+# tup = (1,2,3,4)
+# print(tup[0])
 
-set_converted = list(s)
-print(set_converted[1])
+# set_converted = list(s)
+# print(set_converted[1])
 
-#Arithmetic operations
+# #Arithmetic operations
 
-a = 2
-b = 4
-print(a+b)
-print(a-b)      
-print(a*b)
-print(a/b) 
-print(a!=0)
+# a = 2
+# b = 4
+# print(a+b)
+# print(a-b)      
+# print(a*b)
+# print(a/b) 
+# print(a!=0)
 
-#logical operations
+# #logical operations
 
-print(a>0 and b>0)
-print(a>0 or b<0)
-print(not(a>0))
-number = int(input("Enter a number: "))
-if number==0:
-    print("This is neither even nor odd")
-elif number<0:
-    print("Negative")
-elif number%2==0:
-    print("Even")
-else:
-    print("Odd")
-
-
+# print(a>0 and b>0)
+# print(a>0 or b<0)
+# print(not(a>0))
+# number = int(input("Enter a number: "))
+# if number==0:
+#     print("This is neither even nor odd")
+# elif number<0:
+#     print("Negative")
+# elif number%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
 
 
-my_list = ['css', 'html', 'python', 'javascript', 'ts', 'node']
-my_name = "emmanuel"
-print(my_name.upper())
 
-upper_case_programming_languages = []
-for language in my_list:
-    upper_case_programming_languages.append(language.upper())
-print(upper_case_programming_languages)
 
-print(my_name[::-1])
-print(', '.join(my_list))
-print(my_name.split('n'))
-print(my_name.replace('E', 'e'))
-print(f"My name is {my_name.upper()} and I am learning {my_list[2]}")
+# my_list = ['css', 'html', 'python', 'javascript', 'ts', 'node']
+# my_name = "emmanuel"
+# print(my_name.upper())
 
-print(f"My name is \"{my_name.upper()}\" and I am learning {my_list[2]}")
+# upper_case_programming_languages = []
+# for language in my_list:
+#     upper_case_programming_languages.append(language.upper())
+# print(upper_case_programming_languages)
 
-print('''My name is Bharath \n I want to learn python''')
+# print(my_name[::-1])
+# print(', '.join(my_list))
+# print(my_name.split('n'))
+# print(my_name.replace('E', 'e'))
+# print(f"My name is {my_name.upper()} and I am learning {my_list[2]}")
 
-my_list = ["css", "html", "python", "python","javascript", "ts", "node"]
-my_name = "Bharath"
-print(my_name.swapcase())
+# print(f"My name is \"{my_name.upper()}\" and I am learning {my_list[2]}")
 
-#5th day
-print("Hello World")
+# print('''My name is Bharath \n I want to learn python''')
 
-x=1
-y=0
-try:
+# my_list = ["css", "html", "python", "python","javascript", "ts", "node"]
+# my_name = "Bharath"
+# print(my_name.swapcase())
 
-    print(x/y)
+# #5th day
+# print("Hello World")
 
-except Exception as e:
-    print(f"An error occurred: {e}")
+# x=1
+# y=0
+# try:
+
+#     print(x/y)
+
+# except Exception as e:
+#     print(f"An error occurred: {e}")
 
 #i used total as a global variable and i am trying to update it inside the function but it is giving me an error because i have not declared it as global inside the function. to fix this i need to declare total as global inside the function like this:
